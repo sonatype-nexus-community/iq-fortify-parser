805ac0e69afd9c5ae059d13adc87408c3724240a
chauhanhimanshu <chimanshu@newtglobalcorp.com>
2018-09-10 18:44:06 -0500

    Checking code with new git user name

--- src/main/java/com/thirdparty/ScanGenerator.java
+++ src/main/java/com/thirdparty/ScanGenerator.java
@@ -55,7 +55,7 @@ public class ScanGenerator {
     static final DateDeserializer DATE_DESERIALIZER = new DateDeserializer();
     private static final Charset charset = StandardCharsets.US_ASCII;
 
-    
+
     // GenPriority should exactly copy values from com.fortify.plugin.api.BasicVulnerabilityBuilder.Priority
     // We don't use the original Priority here because we don't want generator to be dependent on the plugin-api
     public enum GenPriority {
@@ -255,7 +255,7 @@ public class ScanGenerator {
         jsonGenerator.writeStringField(DESCRIPTION.attrName(), fn.getDescription());
         jsonGenerator.writeStringField(COMMENT.attrName(), fn.getComment());
         jsonGenerator.writeStringField(BUILD_NUMBER.attrName(), fn.getBuildNumber());
-       
+
         jsonGenerator.writeStringField(REPORT_URL.attrName(), fn.getReportUrl());
         jsonGenerator.writeStringField(NAME.attrName(), fn.getName());
         jsonGenerator.writeStringField(GROUP.attrName(), fn.getGroup());
@@ -270,12 +270,12 @@ public class ScanGenerator {
         jsonGenerator.writeStringField(CVECVSS2.attrName(),DemicalConverter.convertToString(fn.getCvecvss2()));
         jsonGenerator.writeStringField(SONATYPECVSS3.attrName(),DemicalConverter.convertToString(fn.getSonatypecvss3()));
         jsonGenerator.writeStringField(CWECWE.attrName(),DemicalConverter.convertToString(fn.getCwecwe()));
-        
+
         jsonGenerator.writeStringField(CWEURL.attrName(),fn.getCweurl());
         jsonGenerator.writeStringField(LAST_CHANGE_DATE.attrName(), DATE_SERIALIZER.convert(fn.getLastChangeDate()));
         jsonGenerator.writeStringField(ARTIFACT_BUILD_DATE.attrName(), DATE_SERIALIZER.convert(fn.getArtifactBuildDate()));
         jsonGenerator.writeFieldName(TEXT_BASE64.attrName());
-            writeLoremIpsum(fn.getTextBase64(), jsonGenerator);
+        writeLoremIpsum(fn.getTextBase64(), jsonGenerator);
 
         jsonGenerator.writeEndObject();
     }
