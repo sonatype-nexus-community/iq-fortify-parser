2222c9ad0a3ceb02bf3b7ffe6f2d0d7666d0ffeb
Hervé Boutemy <hboutemy@sonatype.com>
2020-07-27 17:21:25 +0200

    add scan integration test: given a reference SBOM to import to IQ and a reference scan result, a unit test runs the integration and checks that the effective scan result matches

--- sonatype-fortify-integration/src/test/java/com/sonatype/ssc/intsvc/TestSonatypeController.java
+++ sonatype-fortify-integration/src/test/java/com/sonatype/ssc/intsvc/TestSonatypeController.java
@@ -12,10 +12,20 @@
  */
 package com.sonatype.ssc.intsvc;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
+import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,20 +36,25 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import com.sonatype.ssc.intsvc.service.IQFortifyIntegrationService;
 import com.sonatype.ssc.intsvc.util.ApplicationPropertiesLoader;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = TestApplication.class)
 @ContextConfiguration(classes = IQFortifyIntegrationService.class)
 public class TestSonatypeController
 {
+  private static final String SCAN_JSON_FILENAME = "sonatype-fortify-integration-sample-application_build.json";
   @Autowired
   private IQFortifyIntegrationService iqFortifyIntgSrv;
 
   private static final Logger logger = Logger.getRootLogger();
 
   @Test
-  public void testStartScanLoad() {
+  public void testStartScanLoad() throws Exception {
+    logger.addAppender(new ConsoleAppender(new PatternLayout("%d{ISO8601} [%t] %-5p %c %x - %m%n")));
     ApplicationProperties myProp = null;
     try {
       myProp = ApplicationPropertiesLoader.loadProperties();
@@ -57,13 +72,111 @@ public class TestSonatypeController
     catch (IOException e) {
       logger.error("IOException exception:" + e.getMessage());
     }
+
     if (myProp.getMissingReqProp()) {
-      try {
-        iqFortifyIntgSrv.startLoad(myProp, null, false);
-      }
-      catch (IOException e) {
-        logger.error(e.getMessage());
-      }
+      logger.fatal("missing required property, stopping test");
+      return;
     }
+
+    File jsonFile = new File(SCAN_JSON_FILENAME);
+    if (jsonFile.exists()) {
+      logger.info("Deleting existing " + SCAN_JSON_FILENAME + ": " + jsonFile.delete());
+    }
+
+    try {
+      iqFortifyIntgSrv.startLoad(myProp);
+    }
+    catch (IOException e) {
+      logger.error("issue while running startLoad", e);
+    }
+
+    // check generated sonatype-fortify-integration-sample-application_build.json
+    // content against reference
+    // src/test/resources/Sonatype-Fortify-integration-sample-application-scan.json
+    if (!jsonFile.exists()) {
+      logger.error("Cannot find " + SCAN_JSON_FILENAME + " for integration test");
+      fail("Cannot find " + SCAN_JSON_FILENAME + " for integration test: issues when calling IQ or SSC?");
+    }
+    logger.info("Checking " + SCAN_JSON_FILENAME + " content (" + jsonFile.length() + " bytes)");
+    JSONObject json = (JSONObject) new JSONParser().parse(new FileReader(jsonFile));
+
+    File jsonReferenceFile = new File("src/test/resources/Sonatype-Fortify-integration-sample-application-scan.json");
+    JSONObject ref = (JSONObject) new JSONParser().parse(new FileReader(jsonReferenceFile));
+
+    String current = null;
+    try {
+      JSONArray findings = (JSONArray) json.remove("findings");
+      JSONArray refFindings = (JSONArray) ref.remove("findings");
+
+      // compare base scan fields
+      // obviously not reproducible field: TODO check format
+      json.remove("scanDate");
+      ref.remove(".scanDate");
+      // check other fields
+      @SuppressWarnings("unchecked")
+      Set<Map.Entry<String, Object>> values = ref.entrySet();
+      for(Map.Entry<String,Object> entry: values) {
+        assertEquals(entry.getKey(), entry.getValue(), json.remove(entry.getKey()));
+      }
+
+      // compare findings
+      logger.info("Checking " + findings.size() + " findings");
+      // prepare a map fo reference findings indexed on fileName
+      Map<String, JSONObject> findingsMap = new HashMap<>();
+      for(Object o: refFindings) {
+        JSONObject finding = (JSONObject) o;
+        String key = getFindingKey(finding);
+        Object prev = findingsMap.put(key, finding);
+        if (prev != null) {
+          logger.warn("Findings contains multiple entries with fileName = " + key);
+        }
+      }
+      for(Object o: findings) {
+        JSONObject finding = (JSONObject) o;
+        current = getFindingKey(finding);
+        JSONObject refFinding = findingsMap.remove(current);
+        if (refFinding == null) {
+          assertTrue("Finding with key = " + current, false);
+        }
+        compareFinding(refFinding, finding);
+      }
+      current = null;
+
+      assertEquals("unexpected missing findings: " + findingsMap, 0, findingsMap.size());
+      logger.info("Findings ok");
+
+      assertEquals("unexpected additional fields: " + json, 0, json.size());
+      
+    } catch (AssertionError ae) {
+      if (current == null) {
+        logger.error("there was a failure during checks: see JUnit output");
+      } else {
+        logger.error("there was a failure while checking finding with fileName = " + current);
+      }
+      throw ae;
+    }
+  }
+
+  private void compareFinding(JSONObject refFinding, JSONObject finding) {
+    // obviously not reproducible field: TODO check format
+    refFinding.remove(".uniqueId");
+    finding.remove("uniqueId");
+    // simplified comparison
+    refFinding.remove("description");
+    finding.remove("description");
+    refFinding.remove("vulnerabilityAbstract");
+    finding.remove("vulnerabilityAbstract");
+
+    // TODO cveurl and reportUrl start with IQ url
+
+    @SuppressWarnings("unchecked")
+    Set<Map.Entry<String, Object>> values = refFinding.entrySet();
+    for(Map.Entry<String,Object> entry: values) {
+      assertEquals(entry.getKey(), entry.getValue(), finding.remove(entry.getKey()));
+    }
+  }
+
+  private String getFindingKey(JSONObject json) {
+    return (String) json.get("issue") + " - " + (String) json.get("fileName");
   }
 }
